484f81b94ad7a15bb150bd6b42f8b9db7d1cc366
Mike <poizon1987@gmail.com>
2022-06-11 16:52:37 -0700

    finished day counter and rollover feature

--- time_calculator.py
+++ time_calculator.py
@@ -14,19 +14,16 @@ def add_time(start_time,added_time,weekday=None):
     minutes = 0
     mil_start = 0
     total_mil_hours = 0
-
     weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-
-
     calc_minutes = int(st_minute) + int(add_minute)
 
     while calc_minutes >= 60:
         calc_minutes = calc_minutes - 60
         over_count = over_count + 1
     if calc_minutes < 10:
-        minutes = f"{zero}{calc_minutes}"#
-    else:#
-        minutes = calc_minutes#  minutes done
+        minutes = f"{zero}{calc_minutes}"
+    else:
+        minutes = calc_minutes
 
     if of_day == 'PM':
         mil_start = int(st_hour) + 12
@@ -50,19 +47,37 @@ def add_time(start_time,added_time,weekday=None):
     if total_mil_hours == 0:
         total_mil_hours = 12
 
-    
     if day_count > 1:
-        days = f"{' ('}{day_count}{' days later'}{')'}"
+        days = f"{'('}{day_count}{' days later'}{')'}"
     elif day_count == 1:
         days = 'next day'
+    elif day_count == 0:
+        days = ''
 
-    print(f"{total_mil_hours}:{minutes} {of_day}{','}{days}")
-    #print(total_mil_hours)
-    #print(hours)
+    for day in weekdays:
+        what_day = str(weekdays).lower().replace("'","").replace("[","").replace("]","").replace(" ","")
+
+    if weekday != None:
+        weekday = weekday.lower()
+
+        if weekday in what_day:
+            the_day = what_day.split(",")
+            day_pos = the_day.index(weekday)
+            new_day_pos = day_pos + day_count
+            while new_day_pos > 6:
+                new_day_pos = new_day_pos - 7
+
+            print(f"{total_mil_hours}:{minutes} {of_day}{', '}{weekdays[new_day_pos]} {days}")
+
+    else:
+        print(f"{total_mil_hours}:{minutes} {of_day} {days}")
+
 
 
 #add_time("3:00 PM", "3:10")
+#add_time("11:30 AM", "2:32", "Monday")
 #add_time("11:43 AM", "0:20")
-#add_time("10:10 PM", "3:30")
+add_time("10:10 PM", "3:30")
 #add_time("11:43 PM", "24:20", "tueSday")
-add_time("6:30 PM", "205:12")
+#add_time("6:30 PM", "205:12")
+#add_time("6:30 PM", "205:12", "tuesday")
